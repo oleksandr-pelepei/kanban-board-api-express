ec2d5c8bd080b9db528325d9ca1df9b3d9547e44
Oleksandr Pelepey <sasha.pelepy@gmail.com>
2017-08-09 12:16:08 +0300

    Added user email validation.

--- app/models/User.js
+++ app/models/User.js
@@ -16,7 +16,14 @@ var schema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function(v) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(v);
+      },
+      message: '{VALUE} is not a valid email.'
+    }
   },
   password: {
     type: String,
